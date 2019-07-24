675f6ea6ca9008c460b0d93c28fcb35743520317
Krishna Upadhyay <072bct517krishna@ioe.edu.np>
2019-07-24 14:49:07 +0545

    Removed unused Imports

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Route, Link, Switch } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
-import Layout from '../layout'
 
 const App = () => (
   <>
